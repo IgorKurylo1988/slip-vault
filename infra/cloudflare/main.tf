7d858f5819581c6fee23e9bc51436ca477c346a7
igor.kurylo <ikurylo22@gmail.com>
2026-06-28 13:04:20 +0300

    try to fixing somethiing

--- infra/cloudflare/main.tf
+++ infra/cloudflare/main.tf
@@ -11,14 +11,12 @@ data "cloudflare_zone" "zone" {
 
 # Create the Cloudflare Pages project
 resource "cloudflare_pages_project" "web_app" {
-  account_id        = var.cloudflare_account_id
   name              = local.project_name
   production_branch = "main"
 }
 
 # Bind the custom root domain to the Pages project
 resource "cloudflare_pages_domain" "root" {
-  account_id   = var.cloudflare_account_id
   project_name = cloudflare_pages_project.web_app.name
   name         = local.dns_name
 }
